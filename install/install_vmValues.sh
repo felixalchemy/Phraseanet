e4936055ca045bb7a2fb946349b5b563e12374b3
felixalchemy <poimlk!>
2020-07-27 19:13:58 +0200

    autoinstall bash update

--- install/install_vmValues.sh
+++ install/install_vmValues.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # set vm.max_map_count value for ElasticSearch
 # see https://www.elastic.co/guide/en/elasticsearch/reference/current/docker.html#docker-prod-prerequisites
-display_title "Set vm.max_map_count new value"
+display_title "vm.max_map_count value check"
 VM_MAX_MAP_COUNT=`sysctl --values --ignore vm.max_map_count`
 
 if [[ "$VM_MAX_MAP_COUNT" != "262144" ]]; then
@@ -18,4 +18,13 @@ if [[ "$VM_MAX_MAP_COUNT" != "262144" ]]; then
           # sudo sysctl -w vm.max_map_count=262144
           echo "macOS"
   fi
+fi
+
+VM_MAX_MAP_COUNT=`sysctl --values --ignore vm.max_map_count`
+if [[ "$VM_MAX_MAP_COUNT" != "262144" ]]; then
+
+  echo "Can't set vm.max_map_count value. Please set it manually"
+  exit 0
+else
+  echo "vm.max_map_count value OK."
 fi
