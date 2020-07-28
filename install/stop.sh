e3cbe701c6f348c23e5b1220dff9611187067975
felixalchemy <poimlk!>
2020-07-28 17:58:27 +0200

    autoinstall bash update

--- install/stop.sh
+++ install/stop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ABSOLUTE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source ABSOLUTE_DIR/installFunctions.sh
+source $ABSOLUTE_DIR/installFunctions.sh
 
 display_title "Stop Phraseanet app"
 sg docker "docker-compose stop"
